4a4549b64d5b6890d71655f333681eeb22f2d13b
Numeez <numeezbaloch17@gmail.com>
2025-11-25 10:38:39 +0530

    Fix the bug related to the token auth middleware

--- internal/tokens/tokens.go
+++ internal/tokens/tokens.go
@@ -31,7 +31,7 @@ func GenerateToken(userID int, ttl time.Duration, scope string) (*Token, error) 
 		return nil, err
 	}
 	token.PlainText = base32.StdEncoding.WithPadding(base32.NoPadding).EncodeToString(emptyBytes)
-	hash := sha256.Sum256(emptyBytes)
+	hash := sha256.Sum256([]byte(token.PlainText))
 	token.Hash = hash[:]
 	return token, nil
 
